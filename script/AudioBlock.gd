b56e8b793e51b752000b4e43e1db04a110f15037
BeatMil <anutorn@gmail.com>
2019-09-10 00:08:57 +0700

    from laptop with sfx and dead animation

--- script/AudioBlock.gd
+++ script/AudioBlock.gd
@@ -1,6 +1,6 @@
 extends AudioStreamPlayer
 
-onready var player = get_node("../Player")
+#onready var player = get_node("../Player")
 onready var audio = get_node(".")
 func _ready():
 	audio.play()
