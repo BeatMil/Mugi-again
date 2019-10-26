9f4d2f049295f0622547fcd7d55cf25e6f985fd9
BeatMil <anutorn@gmail.com>
2019-10-27 00:12:08 +0700

    added fireball animation (bad code)

--- script/Player.gd
+++ script/Player.gd
@@ -1,6 +1,13 @@
 #Player.gd
 extends KinematicBody2D
 
+# enum that I don't use how to use
+const State = {
+    STATE_IDLE = 0,
+    STATE_PATROL = 1,
+    STATE_ATTACK = 2
+}
+
 # Configuration
 var gravity = 90
 var ground = Vector2(0,-1)
@@ -8,7 +15,7 @@ var jumpPower = 2000
 var walkPower = 500
 
 var is_dead = false
-
+var is_attacking = false
 var velocity = Vector2()
 var direction = 1 	# 1 = facing right
 
@@ -22,6 +29,7 @@ onready var sprite = get_node("Sprite")
 onready var collision = get_node('CollisionShape2D')
 onready var area = get_node('Stand')
 onready var health_bar = $"."/HealthBar
+onready var timer = $"Timer"
 
 # area2D collision changer
 var stand_collision_position = Vector2()
@@ -46,13 +54,14 @@ func _ready():
 	pass
 func _physics_process(delta):
 	if !is_dead:
-		Gravity()
-		Move()
-		Jump()
-		Crouch()
-		velocity = move_and_slide(velocity,ground)
-		attack()
-		dead_check()
+		if !is_attacking:
+			Gravity()
+			Move()
+			Jump()
+			Crouch()
+			velocity = move_and_slide(velocity,ground)
+			attack()
+			dead_check()
 	else:
 		dead()
 		
@@ -105,9 +114,12 @@ func Crouch():
 
 func attack():
 	if Input.is_action_just_pressed("ui_accept"):
+		is_attacking = true
+		timer.start()
 		var fireball = FIREBALL.instance()
 		fireball.set_position($".".get_position() + Vector2(100 * direction,0))
 		$"..".add_child(fireball)
+		anim.play("attack01")
 
 
 	
@@ -149,3 +161,7 @@ func _on_Area2D_body_entered(body):
 func dead_check():
 	if health_bar.hp <= 0:
 		is_dead = true
+
+func _on_Timer_timeout():
+	is_attacking = false
+	pass # Replace with function body.
