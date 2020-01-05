479b30d36a13af2a4db921c8b1746151ce7379ed
BeatMil <anutorn@gmail.com>
2020-01-05 13:40:00 +0700

    creating moving platform

--- script/moving_platform.gd
+++ script/moving_platform.gd
@@ -1,25 +1,25 @@
-extends Sprite
+extends KinematicBody2D
 onready var pos1 = get_parent().get_node("pos1")
 onready var pos2 = get_parent().get_node("pos2")
 var is_at_pos1 = true
 
 func _ready():
 	$".".set_global_position(pos1.global_position)
-	print($".".global_position)
-	print(pos1.global_position)
-	print("...")
+#	print($".".global_position)
+#	print(pos1.global_position)
 
 func _physics_process(delta):
 	position_checker()
 	if is_at_pos1:
-		
+		print("is_at_pos1")
+		move_and_slide(Vector2(100,0), Vector2.ZERO)
 		pass
 
-func position_checker(): 
+func position_checker():
 	# switch boolean if node is at starting position
 	if $".".global_position == pos1.global_position:
 		is_at_pos1 = true
 
 	if $".".global_position == pos2.global_position:
-		is_at_pos1 = false 
+		is_at_pos1 = false
 		
