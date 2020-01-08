1a12bada4647496edc9fbb98c2ae81a36ca88400
beatmil <anutorn@gmail.com>
2020-01-08 17:58:25 +0700

    linux

--- script/fish_around.gd
+++ script/fish_around.gd
@@ -5,6 +5,8 @@ extends Node2D
 # var b = "text"
 var num1 = 0
 var num2 = 10
+var pos1 = Vector2(0,0)
+var pos2 = Vector2(100,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #	print(lerp(10, 20, 2))
@@ -13,7 +15,6 @@ func _ready():
 	
 func _physics_process(delta):
 	num1 = lerp(num1, num2, 0.75)
-	print(num1)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#	print(num1)
+	var box = lerp(pos1, pos2, 0.75)
+	print(box)
--- script/moving_platform.gd
+++ script/moving_platform.gd
@@ -1,25 +1,31 @@
 extends KinematicBody2D
-onready var pos1 = get_parent().get_node("pos1")
-onready var pos2 = get_parent().get_node("pos2")
+onready var pos1 = get_parent().get_node("pos1").global_position
+onready var pos2 = get_parent().get_node("pos2").global_position
+#export var pos3 = get_node()
 var is_at_pos1 = true
 
 func _ready():
-	$".".set_global_position(pos1.global_position)
+	$".".set_global_position(pos1)
 #	print($".".global_position)
-#	print(pos1.global_position)
-
+	pass
+	
 func _physics_process(delta):
 	position_checker()
 	if is_at_pos1:
-		print("is_at_pos1")
-		move_and_slide(Vector2(100,0), Vector2.ZERO)
+#		print("is_at_pos1")
+#		move_and_slide(Vector2(100,0), Vector2.ZERO)
+		move_and_slide(get_vector(), Vector2.ZERO)
 		pass
+		
+func get_vector():
+	return (pos2 - pos1) * 0.2
+	pass
 
 func position_checker():
 	# switch boolean if node is at starting position
-	if $".".global_position == pos1.global_position:
+	if $".".global_position == pos1:
 		is_at_pos1 = true
 
-	if $".".global_position == pos2.global_position:
+	if $".".global_position == pos2:
 		is_at_pos1 = false
 		
