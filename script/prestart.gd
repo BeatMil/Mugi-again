ada887b6cd0ab74dc26e28cef3b4ed793c16fe2c
beatmil <anutorn@gmail.com>
2020-02-12 13:58:10 +0700

    added prestart scene

---
+++ script/prestart.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _input(event):
+	if Input.is_key_pressed(KEY_SPACE):
+		get_tree().change_scene("res://scene/dew_intro.tscn")
