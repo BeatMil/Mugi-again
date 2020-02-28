86b6cd46d3ea0289382caeca8cdc55cf2b64a90f
BeatMil <anutorn@gmail.com>
2020-02-28 14:03:11 +0700

    added this is easy we kill them and then we go home

--- script/csgo.gd
+++ script/csgo.gd
@@ -1,6 +1,20 @@
 extends Node2D
 
+#cache
+const okay_lets_go = preload("res://media/Sound/csgo/csgo/we_kill_them_then_we_go_home.ogg")
+
+
+func playsfx(player,sfx):
+	player.set_stream(sfx)
+	player.play()
+	
+
 func _ready() -> void:
+	playsfx($"/root/SfxBlock",okay_lets_go) 
 	if $".".has_node("Control"):
-		print_debug()
 		$Control/revolver.grab_focus()
+#		print($Control/revolver.has_focus())
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed():
+		print(event.as_text())
