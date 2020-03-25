a16ddfb0064acdc10a86aaf7315b0a0d8915f7e2
BeatMil <anutorn@gmail.com>
2020-03-25 10:16:29 +0700

    added health bar animation at the start of boss fight  Changes to be committed: 	modified:   prefab/HealthBar.tscn 	modified:   scene/boss_fight.tscn 	modified:   script/boss_fight.gd

--- script/boss_fight.gd
+++ script/boss_fight.gd
@@ -2,11 +2,22 @@ extends Node2D
 
 var animation_list = []
 var i = 0 # iterate for animation_list
+var start_health_bar_increase = false #health bar animation at start
 
 func _ready() -> void:
+	$boss_healthbar.set_value(0)
+	$AnimationPlayer.play("intro_health_increase")
+#	$boss_health_timer.start()
 	$Timer.start()
 	for i in $yggdrasil/AnimationPlayer.get_animation_list():
 		animation_list.append(i)
+
+func _physics_process(delta: float) -> void:
+	pass
+#	if !start_health_bar_increase:
+#		$boss_health_timer.start()
+#		start_health_bar_increase = true
+	
 
 
 
@@ -21,3 +32,11 @@ func _on_Timer_timeout() -> void:
 func _on_death_border_area_entered(area: Area2D) -> void:
 	if area.is_in_group("dew"):
 		print("RESTART THE GAME!")
+
+
+func _on_boss_health_timer_timeout() -> void:
+	if $boss_healthbar.get_value() < 100:
+		$boss_healthbar.value += 1
+	else:
+		print_debug()
+		$boss_health_timer.stop()
